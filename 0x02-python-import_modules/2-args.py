1f8544131879ac3c017cc12c6f106582ae1fbd4b
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-06-02 16:49:56 +0100

    added conditon

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print(len(sys.argv) - 1, "arguments.")
     elif len(sys.argv) > 1:
-        print(len(sys.argv) - 1, "arguments:")
+        if len(sys.argv) == 2:
+            print(len(sys.argv) - 1, "argument:")
+        else:
+            print(len(sys.argv) - 1, "arguments:")
         for i in range(1, len(sys.argv)):
-            print("{:d}: {:s}".format(i, sys.argv[i]))
+            print("{:d}: {}".format(i, sys.argv[i]))
