312de80a725868fd2fed47323996e35b09a24ec9
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-06-06 21:45:15 +0000

    CPython

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,35 @@
+#include "Python.h"
+#include "listobject.h"
+#include "object.h"
+
+/**
+ * print_python_list_info - Print info about 
+ * Python lists
+ * 
+ * @p: List struct
+ */
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t i;
+	PyObject *item;
+	PyListObject *input;
+	PyTypeObject *typ;
+
+	if (PyList_Check(p))
+	{
+		input = (PyListObject *)(p);
+		printf("[*] Size of the Python List = %zu\n", PyList_Size(p));
+		printf("[*] Allocated = %zu\n", input->allocated);
+
+		if (PyList_Size(p) > 0)
+		{
+			for (i = 0; i < PyList_Size(p); i++)
+			{
+				item = PyList_GetItem(p, i);
+				typ = item->ob_type;
+				printf("Element %zu: %s\n", i, typ->tp_name);
+			}
+		}
+	}
+}
+
