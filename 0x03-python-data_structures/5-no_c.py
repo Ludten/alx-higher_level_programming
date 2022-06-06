2d648fce2b72efdca851af13946fbfbdb8a8778c
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-06-06 03:32:58 +0100

    remove test conditions

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,8 +6,3 @@ def no_c(my_string):
             new_string[i] = ''
     n_string = "".join(new_string)
     return n_string
-
-
-print(no_c("Best School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
