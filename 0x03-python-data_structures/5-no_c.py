ccefdde8df86db3d0fa9e2d23a7ee16568dc5a99
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-06-06 03:31:48 +0100

    correction

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(len(my_string)):
-        if my_string[i] == 'c' or my_string[i] == 'C':
-            new_string = my_string[:i] + my_string[i + 1:]
-    return new_string
+    new_string = list(my_string)
+    for i in range(len(new_string)):
+        if new_string[i] == 'c' or new_string[i] == 'C':
+            new_string[i] = ''
+    n_string = "".join(new_string)
+    return n_string
 
 
 print(no_c("Best School"))
