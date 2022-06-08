2111b6a1f9281930acddfcb8effd07220e6bba50
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-06-08 22:12:40 +0100

    what is the truth

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/uroman_stringr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is not None or (isinstance(roman_string, str)) is False:
+    if roman_string is not None or (isinstance(roman_string, str)) is True:
         roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
                  'M': 1000}
         number = 0
