913bd2db1b9b3df79129c07a6bb82c0a178312e5
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-06-08 20:12:33 +0100

    Average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from functools import reduce
+
+
+def weight_average(my_list=[]):
+    if my_list:
+        s = 0
+        for i in my_list:
+            s += (i[0] * i[1])
+        w = 0
+        for x in my_list:
+            w += x[1]
+        return s / w
+    return 0
