a1077314841b0e678f9921d83cdb8ca6caddc500
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-06-08 22:13:36 +0100

    find issue

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,4 @@
-#!/uroman_stringr/bin/python3
+#!/usr/bin/python3
 def roman_to_int(roman_string):
     if roman_string is not None or (isinstance(roman_string, str)) is True:
         roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
