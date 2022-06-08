cd85f2328830e4dfb643f29502262bc144376690
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-06-08 20:29:21 +0100

    corrected llogic

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,8 +3,8 @@ def complex_delete(a_dictionary, value):
     values = a_dictionary.values()
     for val in values:
         if val == value:
+            for i, j in a_dictionary.items():
+                if j == value:
+                    del a_dictionary[i]
             return a_dictionary
-    for i, j in a_dictionary.items():
-        if j == value:
-            del a_dictionary[i]
     return a_dictionary
