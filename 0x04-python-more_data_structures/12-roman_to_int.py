918a8cd6c62223967b8d4ff61e286a28193a990e
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-06-08 22:05:36 +0100

    romans revenge

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/uroman_stringr/bin/python3
+def roman_to_int(roman_string):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+             'M': 1000}
+    number = 0
+    roman_string = roman_string.replace("IV", "IIII").replace("IX", "VIIII")
+    roman_string = roman_string.replace("XL", "XXXX").replace("XC", "LXXXX")
+    roman_string = roman_string.replace("CD", "CCCC").replace("CM", "DCCCC")
+    for char in roman_string:
+        number += roman[char]
+
+    return number
