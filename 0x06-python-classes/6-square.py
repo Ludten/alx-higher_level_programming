bcf69ec5e7e267cff129e7f50fd44c6a23a0f95f
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-06-21 19:55:20 +0100

    Get

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,80 +5,80 @@ A module for working with squares.
 
 
 class Square:
-    """
-    A class used to represent A square
-
-    Args:
-        size (int): length and width of square
-        position (tuple): displacement of sqaure
-
-    Attributes:
-        size (int): where the size is stored
-        position (tuple): where the position is stored
-    """
+    '''Represents a 2D Polygon with 4 equal and perpendicular sides.
+    '''
 
     def __init__(self, size=0, position=(0, 0)):
-        """Initializes the data."""
-        self.__size = size
-        self.__position = position
+        '''Initializes a Square with a given size.
+        Args:
+            size (int): The size of the square.
+            position (tuple): The position of the square.
+        '''
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
-        """
-        Get and set size of square
-
-        Args:
-        value (int): length and width of square
-
-        Raises:
-            TypeError: if value is not an integer
-            ValueError: if value is less than zero
-        """
+        '''Retrieves the size of this Square.
+        Returns:
+            int: The size of this Square.
+        '''
         return self.__size
-
-    @size.setter
-    def size(self, value):
-        """Sets the size to a value."""
-        if isinstance(value, int) is False:
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
 
     @property
     def position(self):
-        """Retrieves the position."""
+        '''Retrieves the position of this Square.
+        Returns:
+            tuple: The position of this Square.
+        '''
         return self.__position
+
+    @size.setter
+    def size(self, value):
+        '''Updates the size of this Square.
+        Args:
+            value (int): The new size of this Square.
+        '''
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        else:
+            if value < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = value
 
     @position.setter
     def position(self, value):
-        """Sets the position to a value."""
-        if not isinstance(value, tuple) or len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if not isinstance(value[0], int) or not isinstance(value[1], int):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        '''Updates the position of this Square.
+        Args:
+            value (tuple): The new position of this Square.
+        '''
+        is_invalid_value = True
+        error_msg = 'position must be a tuple of 2 positive integers'
+        if isinstance(value, tuple):
+            if len(value) == 2:
+                if isinstance(value[0], int) and isinstance(value[1], int):
+                    if value[0] >= 0 and value[1] >= 0:
+                        is_invalid_value = False
+        if is_invalid_value:
+            raise TypeError(error_msg)
+        else:
+            self.__position = value
 
     def area(self):
-        """
-        Calculates the area of the square
-
+        '''Computes the area of this Square.
         Returns:
-            The area
-        """
-        return self.__size * self.__size
+            int: The area of this Square.
+        '''
+        return self.size ** 2
 
     def my_print(self):
-        """
-        Prints the square
-        """
-        if self.__size == 0:
+        '''Prints a 2D table of the '#' symbol with the size of this square
+        based on its position.
+        '''
+        if self.size == 0:
             print('')
         else:
-            print('{}'.format('\n' * self.__position[1]), end='')
+            print('{}'.format('\n' * self.position[1]), end='')
             for i in range(self.size):
-                print('{}{}'.format(
-                    ' ' * self.__position[0], '#' * self.__size))
+                print('{}{}'.format(' ' * self.position[0], '#' * self.size))
