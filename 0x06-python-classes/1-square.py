9a56bc62875eb5bba18456500bf081b15728f1a2
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-06-21 18:06:18 +0100

    Classes & Objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Square:
+    """
+    A class used to represent A square
+
+    Args:
+        size (int): length and width of square
+
+    Attributes:
+        size (int): where the size is stored
+    """
+
+    def __init__(self, size):
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+class Square:
+    """
+    A class used to represent A square
+
+    Args:
+        size (int): length and width of square
+
+    Attributes:
+        size (int): where the size is stored
+    """
+
+    def __init__(self, size=0):
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
