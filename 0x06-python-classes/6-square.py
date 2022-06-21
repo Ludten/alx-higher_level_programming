2fa0ac1aea5033d903d6f749e8a4e4a4523844fb
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-06-21 19:14:02 +0100

    removed number of empty lines

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -79,14 +79,12 @@ class Square:
         """
         if self.__size == 0:
             if self.__position[1] > 0:
-                for i in range(self.__position[1]):
-                    print('')
+                print('')
             print(' ' * self.__position[0], end='')
             print('')
         else:
             if self.__position[1] > 0:
-                for i in range(self.__position[1]):
-                    print('')
+                print('')
             for i in range(self.__size):
                 print(' ' * self.__position[0], end='')
                 for j in range(self.size):
