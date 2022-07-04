b38288f0222a03e0d58a650450b93a7b817c4bfd
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-07-04 20:19:28 +0100

    dict I guess

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -12,8 +12,27 @@ def add_attribute(obj, name, value):
         obj (object)
         name (str)
         value (any)
+
+    Raises:
+        TypeError: If the attribute cannot be added.
     """
-    if hasattr(obj, "__weakref__") is True:
+    if hasattr(obj, "__dict__") is True:
         setattr(obj, name, value)
     else:
         raise TypeError("can't add new attribute")
+
+
+class MyClass():
+    pass
+
+
+mc = MyClass()
+add_attribute(mc, "name", "John")
+print(mc.name)
+
+try:
+    a = "My String"
+    add_attribute(a, "name", "Bob")
+    print(a.name)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
