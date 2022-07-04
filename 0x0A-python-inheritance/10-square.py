1ccca3a2053c3c52c9443c1b3712615cd667b1c8
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-07-04 18:26:03 +0100

    sqr correction

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,58 +2,7 @@
 """
 A module for working with geometry & rectangles.
 """
-
-
-class BaseGeometry:
-    """
-    A class used to represent geometry
-    """
-
-    def area(self):
-        """
-        calculate area of polygon
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        check the integer value
-        """
-        if isinstance(name, str) is not True:
-            raise Exception("'name' must be a string")
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """
-    A class used to represent a rectangle
-
-    Args:
-        width (int): width of rectangle
-        height (int): height of rectangle
-    Attributes:
-        width (int): where the width is being stored
-        height (int): where the height is being stored
-    """
-
-    def __init__(self, width, height):
-        super().__init__()
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """
-        calculate area of polygon
-        """
-        return self.__width * self.__height
-
-    def __str__(self):
-        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
@@ -68,11 +17,6 @@ class Square(Rectangle):
     """
 
     def __init__(self, size):
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-
-    def area(self):
-        return super().area()
-
-    def __str__(self):
-        return super().__str__()
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -17,11 +17,6 @@ class Square(Rectangle):
     """
 
     def __init__(self, size):
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-
-    def area(self):
-        return super().area()
-
-    def __str__(self):
-        return super().__str__()
