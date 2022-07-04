9fc4a558a3e631723c27217d803d14f012417bbe
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-07-04 18:15:28 +0100

    oof

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,29 +2,7 @@
 """
 A module for working with geometry & rectangles.
 """
-
-
-class BaseGeometry:
-    """
-    A class used to represent geometry
-    """
-
-    def area(self):
-        """
-        calculate area of polygon
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        check the integer value
-        """
-        if isinstance(name, str) is not True:
-            raise Exception("'name' must be a string")
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -34,6 +12,7 @@ class Rectangle(BaseGeometry):
     Args:
         width (int): width of rectangle
         height (int): height of rectangle
+
     Attributes:
         width (int): where the width is being stored
         height (int): where the height is being stored
