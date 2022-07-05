f4600b50f2553de6f7b8c9ff39e6bca6db329eb7
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-07-06 00:49:18 +0100

    oof

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -11,25 +11,43 @@ def print_stat(filesize, statcode):
     A function for printing the logs
     """
     print("File size: {:d}". format(filesize))
-    for k, v in statcode.items():
+    for k, v in sorted(statcode.items()):
         if v > 0:
             print("{:s}: {:d}".format(k, v))
 
 
-ln = 0
-file_size = 0
-stat_code = {"200": 0, "301": 0, "400": 0, "401": 0,
-             "403": 0, "404": 0, "405": 0, "500": 0}
 if __name__ == "__main__":
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
+
     try:
         for line in sys.stdin:
-            ln += 1
-            p_line = line.split(" ")
-            if p_line[-2] in stat_code:
-                stat_code[p_line[-2]] += 1
-            file_size += int(p_line[-1])
-            if (ln % 10 == 0):
-                print_stat(file_size, stat_code)
+            if count == 10:
+                print_stat(size, status_codes)
+                count = 1
+            else:
+                count += 1
+
+            line = line.split()
+
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stat(size, status_codes)
+
     except KeyboardInterrupt:
-        print_stat(file_size, stat_code)
+        print_stat(size, status_codes)
         raise
