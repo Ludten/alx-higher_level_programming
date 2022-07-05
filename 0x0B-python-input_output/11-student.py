758772102ee86e30cbdcee60239ec02f19a1a6d6
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-07-05 19:11:31 +0100

    triangle

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+A module that defines a Student class
+"""
+
+
+class Student:
+    """
+    A class that represents a student
+
+    Args:
+        first_name (str)
+        last_name (str)
+        age (int)
+
+    Attributes:
+        first_name (str)
+        last_name (str)
+        age (int)
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        returns dictionary representation of Student
+        class
+
+        Returns:
+            __dict__ (any)
+        """
+        if attrs is not None:
+            new_dict = {}
+            for keys in attrs:
+                for (k, v) in self.__dict__.items():
+                    if k == keys:
+                        new_dict[keys] = self.__dict__[k]
+            return new_dict
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        A method that replaces all the class attributes with
+        those defined in the json file
+
+        Args:
+            json (dict)
+        """
+        for (k, v) in json.items():
+            for (keys, val) in self.__dict__.items():
+                if k == keys:
+                    self.__dict__[k] = json[k]
