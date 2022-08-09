cc2832d60e1c978eca53f2adf962cc9ff2bb7ae3
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-08-09 18:06:16 +0100

    covert charset

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- converts hbtn_0c_0 database to UTF8 in MySQL server
 ALTER DATABASE hbtn_0c_0 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
