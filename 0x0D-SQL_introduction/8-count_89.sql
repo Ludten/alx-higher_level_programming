f9e55809b1339fbb753585f1b1fbf627ac6907cd
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-08-09 15:47:51 +0100

    remove select query

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- count records in the table first_table of the database hbtn_0c_0 in MySQL server
-SELECT id, COUNT(*) FROM first_table WHERE id = 89;
+SELECT COUNT(*) FROM first_table WHERE id = 89;
