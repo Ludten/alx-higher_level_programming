6b48b2b190dbf22ea2f2c465c678985283a0bf79
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-08-10 14:39:27 +0100

    flush

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- creates a MySQL server user user_0d_1 and grant previleges
 CREATE IF NOT EXIST USER 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
