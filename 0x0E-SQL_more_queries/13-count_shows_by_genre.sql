6bb358c82cc2463282557bf776296e4c55f5c4f7
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-08-10 17:54:49 +0100

    genre id

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
 SELECT tg.name as 'genre',
-COUNT(tsg.show_id) AS 'number_of_shows'
+COUNT(tsg.genre_id) AS 'number_of_shows'
 FROM tv_genres AS tg
 INNER JOIN tv_show_genres AS tsg
 ON tg.id = tsg.genre_id
