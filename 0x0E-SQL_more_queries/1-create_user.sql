744e64490c7b08402d3408bd800133bbf1ed0c3f
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-08-10 14:37:51 +0100

    native_pswd

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creates a MySQL server user user_0d_1 and grant previleges
-CREATE IF NOT EXIST USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE IF NOT EXIST USER 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
