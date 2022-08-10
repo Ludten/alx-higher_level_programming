ba93f3749765a7b11eba138265da1ee3c1c54301
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-08-10 15:14:31 +0100

    FULL JOIN

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- lists all cities contained in the database hbtn_0d_usa
+SELECT cities.id, cities.name, states.name
+  FROM cities
+  FULL JOIN states
+  on cities.id = states.id
+      ORDER BY cities.id ASC;
+
