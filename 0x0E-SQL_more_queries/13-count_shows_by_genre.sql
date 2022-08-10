432a8e048f5b4cc978d5d96a2edf67d00e5e0cf1
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-08-10 18:21:52 +0100

    upper case diff

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
-SELECT tg.name as 'genre',
+SELECT tg.name AS 'genre',
 COUNT(tsg.genre_id) AS 'number_of_shows'
-FROM tv_genres AS tg
-INNER JOIN tv_show_genres AS tsg
+FROM tv_genres tg
+INNER JOIN tv_show_genres tsg
 ON tg.id = tsg.genre_id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
