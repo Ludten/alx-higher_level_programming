e112560845e088bdb9cda488465ffac7fd1b0709
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-08-10 17:52:45 +0100

    AS

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
 SELECT DISTINCT tg.name as 'genre',
-COUNT(tsg.show_id) as 'number_of_shows'
+COUNT(tsg.show_id) AS 'number_of_shows'
 FROM tv_genres AS tg
 INNER JOIN tv_show_genres AS tsg
 ON tg.id = tsg.genre_id
--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- lists all genres of the show Dexter
-SELECT tg.name as 'name'
+SELECT tg.name AS 'name'
 FROM tv_genres AS tg
 INNER JOIN tv_show_genres AS tsg
 ON tg.id = tsg.genre_id
