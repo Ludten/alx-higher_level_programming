1792ca0e951d5c188ac8a69d01d5f2ddb3930f6b
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-09-06 21:35:53 +0100

    JS
    Redefined function params

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,17 +6,13 @@ module.exports = class Square extends Rectangle {
     super(size, size);
   }
 
-  charPrint (c) {
-    if (c !== undefined) {
-      for (let i = 0; i < this.height; i++) {
-        let disp = '';
-        for (let j = 0; j < this.width; j++) {
-          disp += c;
-        }
-        console.log(disp);
+  charPrint (c='X') {
+    for (let i = 0; i < this.height; i++) {
+      let disp = '';
+      for (let j = 0; j < this.width; j++) {
+        disp += c;
       }
-    } else {
-      this.print();
+      console.log(disp);
     }
   }
 };
