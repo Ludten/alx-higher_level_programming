4c69eb1e88d6465cdd3c820a6ef4e925c17c70e9
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-09-10 18:33:11 +0100

    MySQLdb
    fixed execute when imported bug

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,26 +4,33 @@ A module that lists all states from a database
 """
 
 import sys
+from unicodedata import name
 import MySQLdb
 
-db = None
-try:
-    db = MySQLdb.connect(
-        host='127.0.0.1',
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-    )
-    db.autocommit = True
-    cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states ORDER BY id")
-    rows = cursor.fetchall()
-    for row in rows:
-        print("{}".format(row))
-except (Exception, MySQLdb.DatabaseError) as error:
-    print(error)
-finally:
-    if db is not None:
-        db.close()
+def mydbsql():
+    db = None
+    try:
+        db = MySQLdb.connect(
+            host='127.0.0.1',
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+        )
+        db.autocommit = True
+        cursor = db.cursor()
+
+        cursor.execute("SELECT * FROM states ORDER BY id")
+        rows = cursor.fetchall()
+        for row in rows:
+            print("{}".format(row))
+    except (Exception, MySQLdb.DatabaseError) as error:
+        print(error)
+    finally:
+        if db is not None:
+            db.close()
+
+
+if __name__ == "__main__":
+    mydbsql()
