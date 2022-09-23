9b9f932fb60412a205e1c8d46c4d9c4bbdfcaa3a
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-09-23 20:10:26 +0100

    Python: Network #1
    Github API for pagination.

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A script that list 10 commits (from the most recent to oldest) of the
+repository “rails” by the user
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    r = requests.get(
+        'https://api.github.com/repos/{}/{}/commits?per_page=10'.format(
+            sys.argv[1], sys.argv[2])
+    )
+    body = r.json()
+    for items in body:
+        if 'sha' in items:
+            print('{}: {}'.format(items['sha'],
+                  items['commit']['author']['name']))
+        else:
+            print(None)
