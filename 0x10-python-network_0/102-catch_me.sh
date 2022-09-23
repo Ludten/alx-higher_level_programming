6968be05a788d36988a0b1db9c2d6a24ece516ac
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-09-23 01:13:22 +0100

    405 error

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!
-curl -H 'Origin: School' -X PUT -s `curl -X PUT -d "user_id=98" -o /dev/null -s -w "%{redirect_url}" $(curl -X PUT -o /dev/null -s -w "%{redirect_url}" 0.0.0.0:5000/catch_me)`
+# curl -H 'Origin: School' -X PUT -s `curl -X PUT -d "user_id=98" -o /dev/null -s -w "%{redirect_url}" $(curl -X PUT -o /dev/null -s -w "%{redirect_url}" 0.0.0.0:5000/catch_me)`
+curl -s 0.0.0.0:5000/catch_me -X PUT -L -d "user_id=98" -H "Origin: School"
