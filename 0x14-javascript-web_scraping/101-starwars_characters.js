22d9ebf9ec9cd022802a21c296ff285ba7ad7327
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2022-10-18 20:42:21 +0100

    JS - Web Scrapping
    used request-promise to ensure it prints accordingly

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,21 +1,32 @@
 #!/usr/bin/node
-const request = require('request');
+const rp = require('request-promise');
 
 const args = process.argv;
 const a = args[2];
 
+async function order (res) {
+  for (let i = 0; i < res.length; i++) {
+    const options = {
+      uri: res[i],
+      json: true // Automatically parses the JSON string in the response
+    };
+
+    await rp(options)
+      .then(function (repos) {
+        console.log(repos.name);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }
+}
+
 try {
-  request('https://swapi-api.hbtn.io/api/films/' + a, (error, response, body) => {
+  rp('https://swapi-api.hbtn.io/api/films/' + a, (error, response, body) => {
     if (error) throw error;
     const obj = JSON.parse(body);
     const res = obj.characters;
-    for (let i = 0; i < res.length; i++) {
-      request(res[i], (error, response, body) => {
-        if (error) throw error;
-        const char = JSON.parse(body);
-        console.log(char.name);
-      });
-    }
+    order(res);
   });
 } catch (error) {
   console.log(error);
