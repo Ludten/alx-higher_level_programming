e1b25aad360689f5a721cd6ed8534de56cd9b77f
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2022-10-18 22:10:59 +0100

    JS - Web Scrapping
    added query to request url

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,18 +6,16 @@ const url = args[2];
 const pdict = {};
 
 try {
-  request(url, (error, response, body) => {
+  request(url + '?completed=true', (error, response, body) => {
     if (error) throw error;
     const obj = JSON.parse(body);
     let ctr = 0;
     for (let i = 0; i < obj.length; i++) {
-      if (obj[i].completed === true) {
-        if (obj[i].userId in pdict) {
-          pdict[obj[i].userId] = ++ctr;
-        } else {
-          ctr = 0;
-          pdict[obj[i].userId] = ++ctr;
-        }
+      if (obj[i].userId in pdict) {
+        pdict[obj[i].userId] = ++ctr;
+      } else {
+        ctr = 0;
+        pdict[obj[i].userId] = ++ctr;
       }
     }
     console.log(pdict);
